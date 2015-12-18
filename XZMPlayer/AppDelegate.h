5ea148b98bacae90f887c64d0b40dd5dd2932604
xiezhimin <xiezhimin@ypwldemac-mini.local>
2015-12-18 14:19:45 +0800

    Initial Commit

---
+++ XZMPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZMPlayer
+//
+//  Created by xiezhimin on 15/12/18.
+//  Copyright © 2015年 ypwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
